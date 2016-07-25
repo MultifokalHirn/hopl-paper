22d3992ec466973e36ab5f63f8160ad1793968d6
Lennard Wolf <lennardwolf@live.de>
2016-07-25 16:00:10 +0200

    tonis kommentare drin als todos

--- tutorial.lisp
+++ tutorial.lisp
@@ -1,15 +1,15 @@
 (defclass person ()
   ((name :accessor person-name
          :initarg :name)
-   (age :accessor person-age ;is the person-* accessor not kind of stupid?
-        :initarg :age)
+   (date-of-birth :accessor person-date-of-birth ;is the person-* accessor not kind of stupid?
+        :initarg :date-of-birth)
    (address :accessor person-address
          :initarg :address)
    (registration-date :accessor person-registration-date
          :initarg :registration-date)))
 
-(defun make-person (name age adress registration-date)
-  (make-instance 'person :name name :age age :address address
+(defun make-person (name date-of-birth address registration-date)
+  (make-instance 'person :name name :date-of-birth date-of-birth :address address
      :registration-date registration-date))
 
 
@@ -17,45 +17,36 @@
   ((bank-details :accessor staffmember-bank-details
             :initarg :bank-details)))
 
-(defun make-staffmember (name age subject adress registration-date bank-details)
-  (make-instance 'staffmember :name name :age age :address address
+(defun make-staffmember (name date-of-birth address registration-date bank-details)
+  (make-instance 'staffmember :name name :date-of-birth date-of-birth :address address
      :registration-date registration-date :bank-details bank-details))
 
 (defclass student (person)
  ((ects-points :accessor student-ects-points
            :initarg :ects-points)))
 
-(defun make-student (name age subject adress registration-date ects-points)
- (make-instance 'student :name name :age age :address address
+(defun make-student (name date-of-birth address registration-date ects-points)
+ (make-instance 'student :name name :date-of-birth date-of-birth :address address
     :registration-date registration-date :ects-points ects-points))
 
-(defgeneric who-am-i (person)
+(defgeneric get-info (person)
       (:documentation "Returns data on all attributes."))
 
-(defmethod who-am-i ((p person))
-    (format t "My name is ~d and I am ~d years old.~%" (person-name p) (person-age p)))
+(defmethod get-info ((p person))
+    (format t "Name: ~d ~% Occupation: ~d ~% Date of Birth: ~d ~% Adress: ~d ~% Date of Registration: ~d ~%" (person-name p) (class-of p) (person-date-of-birth p) (person-address p) (person-registration-date p)))
 
 
-(defmethod who-am-i ((p teacher))
-    (format t "My name is ~d and I am ~d years old.~%" (person-name p) (person-age p))
-    (format t "I am a teacher and my subject is ~d.~%" (teacher-subject p)))
-
-(defmethod who-am-i :around ((p athletic-teacher))
-    (format t "Oh, hi!~%" )
-    (let ((result (call-next-method)))
-      (format t "Gotta sprint back to class, bye-bye!~%")
-      result))
 
 ;-------------------------------------
 
-(defvar *Bill* (make-person 'bill 21 "randomstreet" ))
-(defvar *Johann* (make-staffmember 'johann 42 "funstreet" ))
-(defvar *Susi* (make-athletic-teacher 'susi 30 'tennis 'geography))
+(defvar *Bill* (make-person 'bill 21 "randomstreet" 21222009))
+(defvar *Johann* (make-staffmember 'johann 42 "funstreet" 21222011 "IBAN:1234"))
+(defvar *Susi* (make-student 'susi 30 "funstreet" 21222011 0))
 
 (format t "~%")
-(who-am-i *Bill*)
+(get-info *Bill*)
 (format t "~%--------------~%~%")
-(who-am-i *Johann*)
+(get-info *Johann*)
 (format t "~%--------------~%~%")
-(who-am-i *Susi*)
+(get-info *Susi*)
 (format t "~%")
